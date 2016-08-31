4c22f8918b549ddf8c79d76fbbde1dfa6a72369e
annuraju <bhupatiraju.s@husky.neu.edu>
2016-08-30 23:11:25 -0400

    connect to python server

--- listener.js
+++ listener.js
@@ -1,4 +1,7 @@
 (function(ext) {
+
+    var socket = null;
+
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
@@ -9,15 +12,56 @@
     };
 
     ext.my_first_block = function() {
-      console.log("I am here bitch!!");
-      return "Yes"; 
+      console.log("I am here!!");
+      return "Yes";
     };
+
+    ext.cnct = function () {
+        if(debugLevel)
+            console.log('Connecting to Server');
+        window.socket = new WebSocket("ws://127.0.0.1:9000");
+        window.socket.onopen = function () {
+            var msg = JSON.stringify({
+                "command": "ready"
+            });
+            window.socket.send(msg);
+            if(debugLevel)
+                console.log("Connected!");
+            myStatus = 2;
+            myMsg = 'ready';
+            connected = true;
+        };
+
+        window.socket.onmessage = function (message) {
+            var msg = JSON.parse(message.data);
+            if (debugLevel)
+                console.log(msg);
+            switch (msg['info']) {
+                case 'on':
+                    console.log("ON Button pressed");
+                    break;
+                case 'off':
+                    console.log("OFF Button pressed");
+                    break;
+            }
+        };
+        
+        window.socket.onclose = function (e) {
+            console.log("Connection closed.");
+            socket = null;
+            connected = false;
+            myStatus = 1;
+            myMsg = 'not_ready'
+        };
+    };
+
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             // Block type, block name, function name
             [' ', 'my first block', 'my_first_block'],
+            [' ', 'connect', 'cnct'],
         ]
     };
 
